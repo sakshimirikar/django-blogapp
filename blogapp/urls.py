bbd1f3440d245258489f02dd7c5d84c02a2f63b5
Sakshi <mirikarsakshi2612@gmail.com>
2022-02-14 00:21:30 +0530

    first commit

---
+++ blogapp/urls.py
@@ -0,0 +1,18 @@
+from . import views 
+from django.urls import path
+
+
+app_name='blogapp'
+
+urlpatterns=[
+    
+    # path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail , 
+    # name='post_detail'),
+    # path('' ,views.post_list , name='post_list' ),
+    path('',	views.post_list,	name='post_list'),
+	path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+									views.post_detail,
+									name='post_detail'),
+
+
+]
